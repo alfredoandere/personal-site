99452c97148e95ae407fdb900f7483d759218ca4
Alfredo Andere <andere@berkeley.edu>
2021-07-16 23:07:39 -0700

    more information changes

--- src/data/resume/courses.js
+++ src/data/resume/courses.js
@@ -2,19 +2,19 @@ const courses = [
   {
     title: 'Probability and Random Processes',
     number: 'EE 126',
-		link: 'https://www2.eecs.berkeley.edu/Courses/EE126/',
+    link: 'https://www2.eecs.berkeley.edu/Courses/EE126/',
     university: 'Berkeley',
   },
   {
     title: 'Machine Learning',
     number: 'CS 189',
-		link: 'https://people.eecs.berkeley.edu/~jrs/189/',
+    link: 'https://people.eecs.berkeley.edu/~jrs/189/',
     university: 'Berkeley',
   },
   {
     title: 'Efficient Algorithms and Intractable Problems',
     number: 'CS 170',
-		link: 'https://www2.eecs.berkeley.edu/Courses/CS170/',
+    link: 'https://www2.eecs.berkeley.edu/Courses/CS170/',
     university: 'Berkeley',
   },
   {
@@ -26,23 +26,21 @@ const courses = [
   {
     title: 'Real Analysis',
     number: 'Math 104',
-		link: 'https://math.berkeley.edu/courses/spring-2021-math-104-001-lec',
+    link: 'https://math.berkeley.edu/courses/spring-2021-math-104-001-lec',
     university: 'Berkeley',
   },
   {
     title: 'Cellular and Mollecular Neurobiology',
     number: 'Math 104',
-		link: 'https://classes.berkeley.edu/content/2020-Fall-MCELLBI-160-001-LEC-001',
+    link: 'https://classes.berkeley.edu/content/2020-Fall-MCELLBI-160-001-LEC-001',
     university: 'Berkeley',
   },
   {
     title: 'Neural Computation',
     number: 'Visual Science 265',
-		link: 'https://redwood.berkeley.edu/courses/vs265/',
+    link: 'https://redwood.berkeley.edu/courses/vs265/',
     university: 'Berkeley',
   },
-
-
 ];
 
 export default courses;
